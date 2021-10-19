c52856c7b092bf18c47f0a8a9ace779137558487
Willian Oizumi <oizumi.willian@gmail.com>
2021-10-19 12:47:27 -0300

    initial commit

---
+++ src/main/java/com/logmein/cardgames/CardgamesApplication.java
@@ -0,0 +1,13 @@
+package com.logmein.cardgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardgamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardgamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logmein/cardgames/CardgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logmein.cardgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardgamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
